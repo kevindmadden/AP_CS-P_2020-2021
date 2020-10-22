6121e4981b750f13a08db043af1c228b39619e0f
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-10-22 12:12:06 -0500

    Intro to Functions

--- Period4/u03_IntroToJavascript/s01_TheBeginning.js
+++ Period4/u03_IntroToJavascript/s01_TheBeginning.js
@@ -1,5 +1,5 @@
-
+"use strict";
 function doLotsOfStuff(){
-    document.getElementById("secondParagraph").style.fontSize="50px"
-    document.getElementById("firstParagraph").style.fontSize="50px"
+    document.getElementById("secondParagraph").style.fontSize = "50px"
+    document.getElementById("firstParagraph").style.fontSize = "50px"
 }
