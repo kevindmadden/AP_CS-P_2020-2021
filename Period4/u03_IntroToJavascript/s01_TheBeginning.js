469d0564fb8de9d7efa7439473404751a2e5c42a
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-10-22 11:20:28 -0500

    Intro to Functions

--- Period4/u03_IntroToJavascript/s01_TheBeginning.js
+++ Period4/u03_IntroToJavascript/s01_TheBeginning.js
@@ -1,1 +1,5 @@
-import * as PIXI from 'pixi.js'
+
+function doLotsOfStuff(){
+    document.getElementById("secondParagraph").style.fontSize="50px"
+    document.getElementById("firstParagraph").style.fontSize="50px"
+}
