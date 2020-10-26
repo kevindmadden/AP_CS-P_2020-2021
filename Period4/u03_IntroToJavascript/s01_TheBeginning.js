24d453bf36d7e1f8942446690007e9998fdeb3fd
kevindmadden <31632159+kevindmadden@users.noreply.github.com>
2020-10-26 11:48:15 -0500

    Javscript functions

--- Period4/u03_IntroToJavascript/s01_TheBeginning.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function doLotsOfStuff(){
-    document.getElementById("secondParagraph").style.fontSize = "50px"
-    document.getElementById("firstParagraph").style.fontSize = "50px"
-}
